69d5a9d2741d9efd8bd9fcc05821556f8410ede7
Quatys <quentiniaeny@gmail.com>
2022-12-10 11:18:38 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - Entry point
+ * main - returns a combination of number
  *
- * Return: Always 0
+ * Retrun: Always 0
  */
 int main(void)
 {
-	int tho;
-	int hun;
-	int ten;
-	int bas;
+	int i,j;
 
-	for (tho = 0 ; tho < 10 ; tho++)
+	for (i = 0; i < 100; i++)
 	{
-		for (hun = 0 ; hun <= 10 ; hun++)
+		for(j = 0; j < 100; j++)
 		{
-			for (ten = 0 ; ten < 10 ; ten++)
+			if (i < j)
 			{
-				for (bas = 0 ; bas < 10 ; bas++)
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
-					putchar('0' + tho);
-					putchar('0' + hun);
-					putchar(32);
-					putchar('0' + ten);
-					putchar('0' + bas);
-					if (!(tho == 9 && hun == 8))
-					{
-						putchar(',');
-						putchar(32);
-					}
-					bas++;
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
