1b9088432670c17883658fd9c2c06d1261881471
Quatys <quentiniaeny@gmail.com>
2022-12-14 14:11:26 -0800

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * _islower - checks lowercase letter
- *
- * Return: 1 if lowercase - 0 otherwise
+ * _islower - check for lower case letter
+ *  @c : character to check the case
+ *  Return: 0 or 1
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (c >= 97 && c <= 122);
 }
