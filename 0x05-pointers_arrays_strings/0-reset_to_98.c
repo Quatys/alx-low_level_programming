6cf831a8197ef258d03e6cc2ec2ae6fddc6ff5d5
Quatys <quentiniaeny@gmail.com>
2022-12-19 13:20:01 -0800

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,9 @@
 
 /**
  * reset_to_98 - takes a pointer to a parameter and updates its value.
- * @n: input integer
- * Return: n
+ * @n: input integer.
+ * Return: no return.
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
