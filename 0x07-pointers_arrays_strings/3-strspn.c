1a53559e5bb862b5609a6ea26f87a9c14a70bd99
Quatys <quentiniaeny@gmail.com>
2023-01-03 11:42:35 -0800

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strspn - Entry point
  * @s: input
@@ -10,6 +9,8 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
 	int r;
+
+
 	while (*s)
 	{
 		for (r = 0; accept[r]; r++)
