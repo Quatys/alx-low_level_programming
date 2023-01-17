d32b02e82d39ad1ed5023e595a0c17082a1d7384
Quatys <quentiniaeny@gmail.com>
2023-01-17 02:39:50 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-int _putchar(char c);
 /**
  * struct dog - Initialize struct dog
  * @name:name type char
@@ -11,12 +10,12 @@ int _putchar(char c);
  * Description: name, age and owner of a dog
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef dog dog_t;
