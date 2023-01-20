a77538eddab3043248555a0c00cf270a1090f55a
Quatys <quentiniaeny@gmail.com>
2023-01-20 00:54:33 -0800

    Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - prints its own opcodes
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: Always 0 (Success)
+ * main - prints opcode of own main function
+ * @argc: argument count
+ * @argv: arg value
+ * Return: int
  */
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *arr;
+	int x, i;
+	unsigned char *p;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
-	bytes = atoi(argv[1]);
-
-	if (bytes < 0)
+	x = atoi(argv[1]);
+	if (x < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
+	p = (unsigned char *)main;
+	i = 0;
+	if (x > 0)
 	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx ", arr[i]);
+		while (i < (x - 1))
+			printf("%02hhx ", p[i++]);
+		printf("%hhx\n", p[i]);
 	}
 	return (0);
 }
